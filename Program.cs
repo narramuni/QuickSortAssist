fb6c30d7d5c18eb77fab4b23243da0ba2f342237
narramuni <nmspavan@gmail.com>
2023-10-18 14:06:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        static void runApp()
+        {
+
+            int[] marks = new int[5];
+            marks[0] = 76;
+            marks[1] = 82;
+            marks[2] = 92;
+            marks[3] = 96;
+            marks[4] = 86;
+
+            QuickSort.Sort(marks, 0, marks.Length - 1);
+
+
+            Console.WriteLine("Sorted List of Marks in Ascending Order (Quick Sort):");
+            foreach (var mark in marks)
+            {
+                Console.WriteLine($"Mark: {mark}");
+            }
+
+            Console.ReadLine();
+
+            //QuickSort.Sort(marks, 0, marks.Length - 1);
+
+            //Console.WriteLine("Sorted List of Marks in Ascending Order (Quick Sort):");
+            //foreach (var mark in marks)
+            //{
+            //    Console.WriteLine($"Mark: {mark}");
+            //}
+
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortAssist
+{
+    public class QuickSort
+    {
+
+       
+            public static void Sort(int[] arr, int low, int high)
+            {
+                if (low < high)
+                {
+                    int pivot = Partition(arr, low, high);
+                    if (pivot > 1)
+                    {
+                        Sort(arr, low, pivot - 1);
+                    }
+                    if (pivot + 1 < high)
+                    {
+                        Sort(arr, pivot + 1, high);
+                    }
+                }
+            }
+
+            private static int Partition(int[] arr, int low, int high)
+            {
+                int pivot = arr[low];
+                while (true)
+                {
+                    while (arr[low] < pivot)
+                    {
+                        low++;
+                    }
+
+                    while (arr[high] > pivot)
+                    {
+                        high--;
+                    }
+
+                    if (low < high)
+                    {
+                        Swap(arr, low, high);
+                    }
+                    else
+                    {
+                        return high;
+                    }
+                }
+            }
+
+            private static void Swap(int[] arr, int left, int right)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+    
+
+}
